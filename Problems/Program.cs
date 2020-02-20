54536c9cfe429a02c9da3def875d50c4e5821df0
g18g0723 <g18g0723@HUG131>
2020-02-20 15:42:08 +0200

    Problems.cs finished unit test file created

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,45 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Prefix("hello there ."));
+            Console.ReadLine();
+        }
+
+        public static string Prefix(string str)
+        {
+            if(str =="")
+            {
+                return "null";
+            }
+            else
+            {
+                int strLeng = str.Length;
+                int numWords = 0;
+                string[] strArr = str.Split();
+                string[] symbols = new string[33]{"0","1","2","3","4","5","6","7","8","9","!","@","#","$","%","^","&","*","(",")","{","}","[","]",":","|","<",">",",","/","?","."," "};
+                for(int i = 0; i<strArr.Length; i+=1 )
+                {
+                    bool isword = true;
+                    for(int j = 0; j<symbols.Length; j+=1 )
+                    {
+                        if(strArr[i].Equals(symbols[j]))
+                        {
+                            isword=false;
+                        }
+                        
+                    }
+                    if(isword==true)
+                    {
+                        numWords+=1;
+                    }
+
+                }
+                return strLeng+","+numWords+":"+str;
+            }
         }
     }
 }
