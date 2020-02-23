f1e640d4cf5da5b531f5e51d0cfd0ff5f828261d
g18g0723 <g18g0723@HUG131>
2020-02-23 19:05:36 +0200

    working program

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -22,29 +22,41 @@ namespace Problems
                 int strLeng = str.Length;
                 int numWords = 0;
                 string[] strArr = str.Split();
-                string[] symbols = new string[33]{" ","0","1","2","3","4","5","6","7","8","9","!","@","#","$","%","^","&","*","(",")","{","}","[","]",":","|","<",">",",","/","?","."};
+                
                 for(int i = 0; i<strArr.Length; i+=1 )
                 {
                     
-                    bool isword = true;
-                    for(int j = 0; j<33; j+=1 )
+                    if(isWord(strArr[i]))
                     {
-                        
-                        if(strArr[i].Contains(symbols[j]))
-                        {
-                            isword=false;
-                            
-                        }
-                        
-                    }
-                    if(isword==true)
-                    {
-                        numWords+=1;
+                        numWords +=1;
                     }
 
                 }
                 return strLeng+","+numWords+":"+str;
             }
+        }
+
+        private static bool isWord(string str)
+        {
+            string[] symbols = new string[32]{" ","0","1","2","3","4","5","6","7","8","9","!","@","#","$","%","^","&","*","(",")","{","}","[","]",":","|","<",">",",","/","?"};
+            bool isword = true;
+            for(int j = 0; j<32; j+=1 )
+            {
+                if(str.Equals(""))
+                {
+                    isword=false;
+                }        
+                else if(str[0].Equals(symbols[j]))
+                {
+                    isword=false;            
+                }
+                        
+            }
+            if(isword==true)
+            {
+               return true;
+            }
+            return false;
         }
     }
 }
