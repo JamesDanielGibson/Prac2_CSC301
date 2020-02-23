84a03d1586f597dd7af98b378b976f436b751425
g18g0723 <g18g0723@HUG131>
2020-02-23 17:48:41 +0200

    test fixed

--- Problems.Tests/Tests.cs
+++ Problems.Tests/Tests.cs
@@ -1,18 +1,34 @@
 using NUnit.Framework;
-
+using Problems;
 namespace Problems.Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
+      
         [Test]
-        public void Test1()
+        public static void Test1()
         {
-            Assert.Pass();
+            //Arrange
+            Program p = new Program();
+            
+            string exp = Program.Prefix("hello");
+            Assert.That( exp, Is.EqualTo("5,1:hello"));
+        }
+        [Test]
+        public static void Test2()
+        {
+            Program p = new Program();
+            
+            string exp = Program.Prefix("");
+            Assert.That( exp, Is.EqualTo("0,0:"));
+        }
+        [Test]
+        public static void Test3()
+        {
+            Program p = new Program();
+            
+            string exp = Program.Prefix("what    ...  did you say?? ");
+            Assert.That( exp, Is.EqualTo("27,5:what    ...  did you say?? "));
         }
     }
 }
